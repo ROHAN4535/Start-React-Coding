2c6453b22eedd9f0f27beae4efb20eec8c17be87
ROHAN4535 <grohan468@gmai.com>
2023-08-18 20:07:23 +0530

    Start React Coding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ExpenseItem></ExpenseItem>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
