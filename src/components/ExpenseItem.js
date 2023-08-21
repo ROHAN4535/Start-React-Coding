7d79d203ab53c91294908564b436c739e3f54405
ROHAN4535 <grohan468@gmai.com>
2023-08-21 14:41:06 +0530

    (Functional Components) Passing Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,16 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    
     return (
-        <div>
-            <div>March 28th 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
 }
 
 export default ExpenseItem;
-
-
-/*function ExpenseItem() {
-    return (
-        <div>
-            <h2>Expense Items</h2>
-            <h3>Food Rs 10</h3>
-            <h3>Petrol Rs 100</h3>
-            <h3>Movies Rs 200</h3>
-        </div>
-        )
-}
-
-export default ExpenseItem;*/
