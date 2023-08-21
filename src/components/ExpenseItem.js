c7f2a62b7ce0503608e1e703533a9eb14a55f4f2
ROHAN4535 <132249763+ROHAN4535@users.noreply.github.com>
2023-08-21 14:04:33 +0530

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,21 @@
 function ExpenseItem() {
     return (
         <div>
+            <div>March 28th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
+
+
+/*function ExpenseItem() {
+    return (
+        <div>
             <h2>Expense Items</h2>
             <h3>Food Rs 10</h3>
             <h3>Petrol Rs 100</h3>
@@ -9,4 +24,4 @@ function ExpenseItem() {
         )
 }
 
-export default ExpenseItem;
+export default ExpenseItem;*/
