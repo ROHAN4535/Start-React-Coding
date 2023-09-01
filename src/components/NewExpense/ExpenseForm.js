bf0e2be134ee47e4a1317524dc644c3ce7f762c1
ROHAN4535 <grohan468@gmail.com>
2023-09-01 20:57:42 +0530

    Submitting The Form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,18 +5,55 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({      //Worked With ONE States
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     enteredDate:'',
+    //})
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+        /* setUserInput((prevState) => {
+            return {...prevState, enteredTitle: event.target.value };
+        });*/
     };
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+        /* setUserInput((prevState) => {
+            return {...prevState, enteredAmount: event.target.value };
+        });*/
     };
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+        /* setUserInput((prevState) => {
+            return {...prevState, enteredDate: event.target.value };
+        });*/
     };
 
-    return <form>
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData)
+    };
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__control">
             <div className="new-expense__control">
                 <label>Title</label>
